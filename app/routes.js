25e383aef161a7b83b5afaf44f6bcf8a00a68c77
SimonW-HE <51440036+SimonW-HE@users.noreply.github.com>
2019-06-14 16:27:03 +0100

    Ineligibility added

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,22 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/juggling-balls-answer', function (request, response) {
+
+  // Make a variable and give it the value from 'juggling-balls'
+  var jugglingBalls = request.session.data['juggling-balls']
+
+  // Check whether the variable matches a condition
+  if (jugglingBalls == "Three or more"){
+    // Send user to next page
+    response.redirect('/juggling-trick')
+  }
+  else {
+    // Send user to ineligible page
+    response.redirect('/ineligible')
+  }
+
+})
+
+
 module.exports = router
